b94bd14821dab2982fc75781beecfb593b336a4a
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-07-02 13:47:26 +0530

    Create design.sv

---
+++ P1/design.sv
@@ -0,0 +1,30 @@
+// Code your design here
+module synchronous_fifo #(parameter DEPTH=8, DATA_WIDTH=8) (
+  input clk, rst_n,
+  input w_en, r_en,
+  input [DATA_WIDTH-1:0] data_in;
+  output reg [DATA_WIDTH-1:0] data_out,
+  output full, empty
+);
+  
+  reg [$clog2(DEPTH)-1:0] w_ptr, r_ptr;
+  reg [DAT_WIDTH-1:0] fifo[DEPTH];
+  
+  always@(posedge clk) begin
+    if(!rst_n) begin
+      w_ptr <= 0;
+      r_ptr <= 0;
+      data_out <= 0;
+    end
+  end
+  
+  always@(posedge clk) begin
+    if(r_en & !empty) begin
+      data_out <= fifo[r_ptr];
+      r_ptr <= r_ptr+1;
+    end
+  end
+  
+  assign full = ((w_ptr+1'b1) == r_ptr);
+  assign empty = (w_ptr == r_ptr);
+endmodule
